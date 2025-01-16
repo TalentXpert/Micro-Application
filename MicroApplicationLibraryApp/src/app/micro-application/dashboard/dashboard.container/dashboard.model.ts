fea10ada07b0f7fe048c6deb3c3585e321102ad1
Aniruddha Chavan <achavan2@medrio.com>
2025-01-16 13:03:59 +0530

    Fixed errors for dashborad models

--- MicroApplicationLibraryApp/src/app/micro-application/dashboard/dashboard.container/dashboard.model.ts
+++ MicroApplicationLibraryApp/src/app/micro-application/dashboard/dashboard.container/dashboard.model.ts
@@ -3,32 +3,32 @@ import { AppControlVM } from "../../form-builder/form.builder.model";
 
 
 export class DashboardGraph {
-    public Type: string;
+    public Type: string="";
 }
 export class DashboardSummaary {
     public DashboardGrids: DashboardGrid[] = [];
 }
 
 export class DashboardGrid {
-    public Title: string;
+    public Title: string="";
     public Headers: DashboardGridHeader[] = [];
     public Rows: GridCell[] = [];
 }
 
 export class DashboardGridHeader {
-    public HeaderIdentifier: string;
-    public Position: number;
-    public HeaderText: string;// visible to  user and can be localized. do not write any logic on it. 
-    public Alignment: string;
-    public DataType: string;
-    public MinWidth: number;// mini width required for this column.
+    public HeaderIdentifier: string="";;
+    public Position: number=0;
+    public HeaderText: string="";// visible to  user and can be localized. do not write any logic on it. 
+    public Alignment: string="";
+    public DataType: string="";
+    public MinWidth: number=0// mini width required for this column.
 }
 
 export class SaveDashboardGridVM {
     public Id?: string;
-    public Name: string;
-    public DataSource: string;
-    public Description: string;
+    public Name: string="";
+    public DataSource: string="";
+    public Description: string="";
     public AppControls: AppControlVM[] = [];
 }
 
@@ -43,6 +43,6 @@ export class DashboardChart {
 }
 export class ChartColumn
 {
-    public Title: string;
-    public DataType: string;
+    public Title: string="";
+    public DataType: string="";
 }
