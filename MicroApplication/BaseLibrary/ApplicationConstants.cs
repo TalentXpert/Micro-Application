0237e9b2f9d576d84667a4dd9249f53ab9aaf485
Archana Chavan <archana.chavan@talentxpert.com>
2025-09-01 10:24:03 +0530

    Updated release version 2025.09.01.01

--- MicroApplication/BaseLibrary/ApplicationConstants.cs
+++ MicroApplication/BaseLibrary/ApplicationConstants.cs
@@ -5,6 +5,6 @@
         public static DateTime? ReleaseDate { get; internal set; }
         public const string JobApproval = "JobApproval";       
         public const string Incomplete = "Incomplete";
-        public static string ReleaseVersion = "2025.08.11.01";
+        public static string ReleaseVersion = "2025.09.01.01";
     }
 }
