d4b7ff6546bfecb0e0c4bedb3fae62de44a9c2e3
smore <smore@ciffconsultants.org>
2025-11-21 12:49:34 +0530

    Add Enum extension

---
+++ MicroApplication/BaseLibrary/Utilities/Extensions/EnumExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace BaseLibrary.Utilities.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            var attribute = enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>();
+
+            return attribute?.GetName() ?? enumValue.ToString();
+        }
+
+        public static T? GetEnumByDisplayName<T>(string displayName) where T : struct, Enum
+        {
+            foreach (var value in Enum.GetValues(typeof(T)).Cast<T>())
+            {
+                var name = ((Enum)(object)value).GetDisplayName();
+                if (name.Equals(displayName, StringComparison.OrdinalIgnoreCase))
+                    return value;
+            }
+
+            return null; // not found
+        }
+    }
+}
