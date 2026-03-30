959313945975017c62eb36e794fc56fea01acd02
Archana Chavan <achavan@ciffconsultants.org>
2026-03-30 10:35:08 +0530

    Updated release version

--- MicroApplication/BaseLibrary/ApplicationConstants.cs
+++ MicroApplication/BaseLibrary/ApplicationConstants.cs
@@ -5,6 +5,6 @@
         public static DateTime? ReleaseDate { get; internal set; }
         public const string JobApproval = "JobApproval";       
         public const string Incomplete = "Incomplete";
-        public static string ReleaseVersion = "2026.03.14.01";
+        public static string ReleaseVersion = "2026.03.30.3";
     }
 }
