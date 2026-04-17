3e56d2e69a09ca901ef85e7b40fa381bcc0aff5d
pkumar1609 <pkumar@talentxpert.com>
2026-04-17 16:40:29 +0530

    Update ApplicationSettingBase.cs

--- MicroApplication/BaseLibrary/ApplicationSettingBase.cs
+++ MicroApplication/BaseLibrary/ApplicationSettingBase.cs
@@ -6,6 +6,7 @@ namespace BaseLibrary
         public static string? WebsiteAdminLoginId { get; set; }
         public static string Environment { get; set; } = string.Empty;
         public static string DatabaseConnectionString { get; set; } = string.Empty;
+        public static string MasterDatabaseConnectionString { get; set; } = string.Empty;
         public static bool IsDatabaseExist { get; set; } 
         public static string DefaultTimeZone { get; set; } = string.Empty;
 
