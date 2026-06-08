754397e65d3d9758d27cfd3134d4000282f1333f
pkumar1609 <pkumar@talentxpert.com>
2026-06-08 09:13:43 +0530

    added method to remove empty string from list.

---
+++ MicroApplication/BaseLibrary/Utilities/Extensions/ListExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace BaseLibrary.Utilities
+{
+    public class ListExtension
+    {
+        public static List<string> RemoveNullString(List<string?> values)
+        {
+            if(values == null)
+                return [];
+            List<string> cleanList = values.OfType<string>().ToList();
+            return cleanList;
+        }
+    }
+}
---
+++ MicroApplication/BaseLibrary/Utilities/Extensions/NextListItem.cs
@@ -0,0 +1,23 @@
+﻿namespace BaseLibrary.Utilities
+{
+
+    public class NextListItem<T>
+    {
+        readonly List<T> ItemList;
+        int index = 0;
+        public NextListItem(List<T> list)
+        {
+            ItemList = list;
+        }
+        public T? GetNext()
+        {
+            if (index < ItemList.Count)
+            {
+                var item = ItemList[index];
+                index += 1;
+                return item;
+            }
+            return default(T);
+        }
+    }
+}
